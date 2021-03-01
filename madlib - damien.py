daa16b7130d10ef5e0d134232e7453703fa675e1
Cyber-Damien <79868832+Cyber-Damien@users.noreply.github.com>
2021-03-01 14:10:49 -0500

    Add files via upload

---
+++ madlib - damien.py
@@ -0,0 +1,14 @@
+print("this will create a story from the things you input")
+
+noun=input("Please input a plural noun: ")
+name=input("Please input a name: ")
+num2=int(input("please input another number: ")
+num=int(input("Please input a number: "))
+name2=input("please input another name: ")
+
+print("You are a streamer named",name,
+      "and you were just finishing up with playing",noun,
+      "for your weekly stream.")
+
+print("While",place, "you run into the evil rogue,",name2,"and shoot,"
+      ,num,noun,"at them")
---
+++ number - Damien.py
@@ -0,0 +1,22 @@
+name = input("Please input your name: ")
+age = int(input("Please input your age: "))
+
+print("Hi", name)
+print("Your age in 2050 is ", age+30)
+
+
+
+num = int(input("Please input number: "))
+
+if num%5 ==0:
+    print(num, "is divisible by 5")
+if num%5 >0:
+    print(num, "is not divisible by 5")
+
+if num%2 ==0:
+    print(num, "is even")
+
+if num%2 >0:
+    print(num, "is odd")
+
+print("Thank you", name)
